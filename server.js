f0fb48beb87503b998cd5ba1554499da6ddbce18
onomatopoetica <72478102+onomatopoetica@users.noreply.github.com>
2021-04-22 16:08:13 -0700

    Deploying

--- server.js
+++ server.js
@@ -27,15 +27,6 @@ mongoose.connect(
   }
 );
 
-// Define API routes here
-
-// Send every other request to the React app
-// Define any API routes before this runs
-// app.get("/", (req, res) => {
-//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
-// });
-
-
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
