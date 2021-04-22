cfb83999a2c6b037a3ca19cdf09d223068c96cb4
onomatopoetica <72478102+onomatopoetica@users.noreply.github.com>
2021-04-22 16:44:11 -0700

    Deploying

--- server.js
+++ server.js
@@ -3,18 +3,21 @@ const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 const routes = require("./routes");
+dotenv.config();
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(routes);
+
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
+app.use(routes);
 
 // Connect to the Mongo DB
 mongoose.connect(
