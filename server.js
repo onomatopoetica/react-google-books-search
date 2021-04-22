f47706198bdb7d8f5c3b87beb263a55001c2539e
onomatopoetica <72478102+onomatopoetica@users.noreply.github.com>
2021-04-22 16:59:51 -0700

    Deploying

--- server.js
+++ server.js
@@ -30,6 +30,10 @@ mongoose.connect(
   }
 );
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
