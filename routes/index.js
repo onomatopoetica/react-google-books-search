8fc31b799f9ebb8b514838bff4f8a0145db4978f
onomatopoetica <72478102+onomatopoetica@users.noreply.github.com>
2021-04-21 18:12:49 -0700

    Deploying

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,5 @@ const apiRoutes = require("./API");
 // API Routes
 router.use("/api", apiRoutes);
 
-// If no API routes are hit, send the React app
-router.use(function (req, res) {
-    // res.sendFile(path.join(__dirname, "../client/public/index.html"));
-    res.sendFile(path.join(__dirname, "../client/build/index.html"));
-});
 
 module.exports = router;
